cb45139a89aca720028302ab1dbc169669d8b99b
tmanet <tmanet@student.42.fr>
2016-02-24 17:33:10 +0100

    norme

--- make_window.c
+++ make_window.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 13:15:12 by tmanet            #+#    #+#             */
-/*   Updated: 2016/02/24 17:06:20 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/02/24 17:32:41 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 t_graph	*make_window(char *str)
 {
-	t_graph	*graph;
+	t_graph	*grp;
 
-	if (!(graph = (t_graph*)ft_memalloc(sizeof(t_graph))))
-		ft_error("graph allocation failed");
-//	ft_map_builder(list, graph);
-	graph->f = &ft_pixel_mendel;
-	graph->width = WIDTH;
-	graph->height = HEIGHT;
-	graph->x_mid = graph->width / 2;
-	graph->y_mid = graph->height / 2;
-	graph->x_offset = graph->x_mid;
-	graph->y_offset = graph->y_mid;
-	ft_zoom_opti(graph);
-	graph->keymap = ft_key_mapping();
-	graph->mlx = mlx_init();
-	graph->win = mlx_new_window(graph->mlx, graph->width, graph->height, str);
-	mlx_key_hook(graph->win, &ft_get_key, graph);
-	mlx_hook(graph->win, BUTTON_NOTIFY, BUTTON1_MOTION_MASK, &ft_get_mouse, graph);
-	mlx_expose_hook(graph->win, &ft_expose, graph);
-	return (graph);
+	if (!(grp = (t_graph*)ft_memalloc(sizeof(t_graph))))
+		ft_error("grp allocation failed");
+//	ft_map_builder(list, grp);
+	grp->f = &ft_pixel_mendel;
+	grp->width = WIDTH;
+	grp->height = HEIGHT;
+	grp->x_mid = grp->width / 2;
+	grp->y_mid = grp->height / 2;
+	grp->x_offset = grp->x_mid;
+	grp->y_offset = grp->y_mid;
+	ft_zoom_opti(grp);
+	grp->keymap = ft_key_mapping();
+	grp->mlx = mlx_init();
+	grp->win = mlx_new_window(grp->mlx, grp->width, grp->height, str);
+	mlx_key_hook(grp->win, &ft_get_key, grp);
+	mlx_hook(grp->win, BUTTON_NOTIFY, BUTTON1_MOTION_MASK, &ft_get_mouse, grp);
+	mlx_expose_hook(grp->win, &ft_expose, grp);
+	return (grp);
 }
