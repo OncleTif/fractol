ba0739f3a70eab73fa56db9419c91e59fb9319bc
tmanet <tmanet@student.42.fr>
2016-03-05 16:23:21 +0100

    -debug

--- make_window.c
+++ make_window.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 13:15:12 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/05 16:05:39 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/05 16:22:16 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,5 @@ t_graph	*make_window(char *str)
 	mlx_key_hook(grp->win, &ft_get_key, grp);
 	mlx_hook(grp->win, BUTTON_NOTIFY, BUTTON1_MOTION_MASK, &ft_get_mouse, grp);
 	mlx_expose_hook(grp->win, &ft_expose, grp);
-	ft_putstr("fin de make");
 	return (grp);
 }
