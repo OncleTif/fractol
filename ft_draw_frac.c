a27dd1ae16d1c34e51d6c9247f542fd75ae30054
tmanet <tmanet@student.42.fr>
2016-03-06 18:08:30 +0100

    norme

--- ft_draw_frac.c
+++ ft_draw_frac.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/22 14:42:55 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/05 18:57:29 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/06 18:08:00 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	ft_draw_frac(t_graph *grp)
 	int	x;
 
 	mlx_clear_window(grp->mlx, grp->win);
-	ft_putnbrendl(grp->zoom);
 	y = 0;
 	while (y < grp->height)
 	{
@@ -30,14 +29,4 @@ void	ft_draw_frac(t_graph *grp)
 		}
 		y++;
 	}
-	ft_print_origin(grp);
-	ft_putstr("origine fct (x1000):");
-	ft_putnbr(grp->r_orig * 1000);
-	ft_putstr(",");
-	ft_putnbrendl(grp->i_orig * 1000);
-	ft_putstr("mouse :");
-	ft_putnbr(grp->x_orig);
-	ft_putstr(",");
-	ft_putnbrendl(grp->y_orig);
-	ft_putnbrendl(grp->iter);
 }
