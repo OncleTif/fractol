58b3a81b367e47e781c7f1a9ebfa0782562c5814
tmanet <tmanet@student.42.fr>
2016-03-06 16:00:06 +0100

    ajout message julia

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 11:57:48 by tmanet            #+#    #+#             */
-/*   Updated: 2016/02/23 14:17:54 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/06 15:59:04 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(int argc, char **argv)
 	}
 	else
 	{
-		ft_putendl("You must define an argment in :\nmandel,");
+		ft_putendl("You must define an argment in :\nmandel, julia");
 	}
 	return (0);
 }
